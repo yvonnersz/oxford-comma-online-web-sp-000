762d05f8f4893059fdbb14a498692ec5941bfcae
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-27 03:00:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array.join()
+  when 2
+    array.join(" and ")
+  else
+    a = array[0..array.length-2].join(", ")
+    a << ", and " + array.last
+  end
 end
