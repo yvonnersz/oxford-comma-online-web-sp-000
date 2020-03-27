49b994f23fb3a66caf67f0bb628e082e394eff05
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-27 02:58:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array.join()
+  when 2
+    array.join(" and ")
+  else
+    b = array[0..array.length-2].join(", ")
+    b << ", and " + array.last
+  end
 end
